3c02fe30d76d7335bc7c3bbb2d934aae60df94fe
faaguil <franchesca.amor.aguilar@emory.edu>
2023-08-14 11:25:44 -0400

    change directory path and added comment

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,8 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
+#Had to change the directory to match my path on my computer
+setwd("/Users/franchescapples/Desktop/EPI 590R/epi590r-in-class/data/raw")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
@@ -18,5 +19,5 @@ nlsy <- nlsy |>
 
 nlsy <- na.omit(nlsy)
 
-setwd("../clean/")
+setwd("/Users/franchescapples/Desktop/EPI 590R/epi590r-in-class/data/clean")
 write_rds(nlsy, "nlsy-complete-cases.rds")
